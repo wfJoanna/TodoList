56d0531dbec7a65b6bf2098a8bed32c6d80a06e3
wfJoanna <1906761905@qq.com>
2021-02-02 16:14:00 +0800

    调了样式

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
-    <input v-model="inputValue"/>
-    <button @click="addTodo">添加任务</button>
-    <ul>
+    <div class="input-todo">
+      <el-input placeholder="添加任务" v-model="inputValue" style="font-size: 20px;"></el-input>
+      <el-button @click="addTodo" icon="el-icon-plus" style="font-size: 15px;margin-left: 5px;color: white;background: #9dad7f;" circle></el-button>
+    </div>
+    <ol class="todo-list">
       <todo-item v-for="(item,index) in todos" :key="index" :itemTitle="item.title" :itemIndex="index"
                  :itemCompleted="item.completed" @toggleItem="toggleTodo" @deleteItem="deleteTodo"></todo-item>
-    </ul>
+    </ol>
   </div>
 </template>
 
@@ -55,5 +57,14 @@ export default {
 </script>
 
 <style>
-
+.todo-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /*看起来没有完全居中的原因是每一项前面有没显示的序号，因为每一项都是display:flex，所以不显示序号*/
+}
+.input-todo{
+  display: flex;
+  width: 25%;
+  margin: auto;
+}
 </style>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,10 +1,10 @@
 <template>
   <li class="todo-item">
     <div class="item-content" @click="toggleMe" :class="{'completed-item':completedData}">
-      <input type="checkbox" v-model="completedData"/>
+      <input class="item-checkbox" type="checkbox" style="border-radius: 50%;" v-model="completedData"/>
       <label>{{ itemTitle }}</label>
     </div>
-    <button @click="deleteMe">删除</button>
+    <i class="el-icon-delete" style="background: #f7f7e8;margin: 2px;" @click="deleteMe"></i>
   </li>
 </template>
 
@@ -37,5 +37,27 @@ export default {
 <style scoped>
 .completed-item {
   text-decoration: line-through;
+  color: #9dad7f;
+}
+
+.todo-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 30%;
+  border: 1px solid #557174;
+  padding: 2px;
+  margin-top: -1px; /*解决了border重叠成2px的问题*/
+  color: #557174;
+  font-size: 30px;
+}
+
+.item-content {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  margin: 0px 2px 0px 0px;
+  background: #f7f7e8;
+  overflow: auto;
 }
 </style>
